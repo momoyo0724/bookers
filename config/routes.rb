225233edbf8549c689ca4728699d5c4794f38497
momoyo0724 <122843585+momoyo0724@users.noreply.github.com>
2023-02-16 23:38:42 +0000

    2023-02-17-start

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
   root 'homes#top'
-#確認のため一旦非表示にする  get '/top' => 'homes#top'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :books
 end
