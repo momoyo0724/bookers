92c31f9b840c06daad39af76f6aebb3700510d67
momoyo0724 <122843585+momoyo0724@users.noreply.github.com>
2023-02-16 05:07:07 +0000

    0216-bookers-to-books-rename

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get 'books/show'
   get 'books/edit'
   root 'homes#top'
-#一旦非表示にする  get '/top' => 'homes#top'
+#確認のため一旦非表示にする  get '/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
